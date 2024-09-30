9f1cec75bb15ac53186edb46015ea00dec398d15
toseiasante <travisowusuoseiasante@gmail.com>
2024-09-30 12:24:14 -0400

    Add files via upload

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,5 @@
-# This is Version 1 of my hello world program
-print("Hello my name is Travis Osei-Asante")
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\iiitr\AppData\Local\Programs\Python\Python312\helloWorld.py
+Hello my name is Travis osei-Asante!
